1a45d4b5a779e5eb43b2b752ab5c9f5c4511fdd0
Carter <cartersteinhoff@gmail.com>
2023-03-13 14:55:23 -0700

    calendly update

--- components/contact.js
+++ components/contact.js
@@ -189,15 +189,13 @@ export default function Contact() {
           )}
         </section>
       </div>
-      <PopupButton
-        url="https://calendly.com/your_scheduling_page"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
-        rootElement={document.getElementById("__next")}
-        text="Click here to schedule!"
-      />
+      {typeof window !== "undefined" && (
+        <PopupButton
+          url="https://calendly.com/adham-tarek"
+          rootElement={document.getElementById("__next")}
+          text="Click here to schedule!"
+        />
+      )}
     </>
   );
 }
