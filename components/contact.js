4d9934c5e95c1c3b498baf97cbbad67c1ddc4228
Kevin Lopez <kevinblakelopez@gmail.com>
2023-03-13 14:26:44 -0700

    trying out calendly

--- components/contact.js
+++ components/contact.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { PopupButton } from "react-calendly";
 
 import sanitizeHtml from "sanitize-html";
 
@@ -188,6 +189,15 @@ export default function Contact() {
           )}
         </section>
       </div>
+      <PopupButton
+        url="https://calendly.com/your_scheduling_page"
+        /*
+         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+         */
+        rootElement={document.getElementById("root")}
+        text="Click here to schedule!"
+      />
     </>
   );
 }
